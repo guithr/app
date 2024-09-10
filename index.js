b537fab30530d08b18ec2f9bcfe8ae507f41d09e
Guilherme Matos <guilherme.thrmatos@gmail.com>
2024-09-10 20:29:01 -0300

    Adding inquirer to project

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-// comentario
+function start() {
 
-console.log('hello world!')
+
+
+    while (true) {
+        let opcao = "sair"
+        switch (opcao) {
+            case "cadastar":
+                console.log("Opção: Cadastrar")
+                break;
+            case "listar":
+                console.log("Opção: Listar")
+                break;
+            case "sair":
+                console.log("Saindo...")
+                return
+        }
+    }
+}
+
+start()
